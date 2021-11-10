6b8e1d3ff4d9aa3fa56d8bd7335290fc29731d7a
Vinícius Felipe <vfb2@cin.ufpe.br>
2021-11-10 03:12:58 -0300

    updates

--- UDPServer.py
+++ UDPServer.py
@@ -1,9 +1,18 @@
 from socket import *
+
 serverPort = 12000
 serverSocket = socket(AF_INET, SOCK_DGRAM)
 serverSocket.bind(('', serverPort))
-print("The server is ready to receive")
 while True:
+
+    print("The server is ready to receive")
     message, clientAddress = serverSocket.recvfrom(2048)
-    modifiedMessage = message.decode().upper()
-    serverSocket.sendto(modifiedMessage.encode(),clientAddress)
+    messageReceived = message.decode()
+    print("Message received from client: " + messageReceived)
+    modifiedMessage = messageReceived.upper()
+    serverSocket.sendto(modifiedMessage.encode(), clientAddress)
+    print("The message has been modified and sent back to the client")
+    serverSocket.close()
+    print("The server is closed")
+    break
+
