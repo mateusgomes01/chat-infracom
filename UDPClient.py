ee07ad16e8e79913b41f5da2215dfe01f6b9bff3
Vinícius Felipe <56048036+Viniciusfelp@users.noreply.github.com>
2021-11-13 21:21:18 -0300

    update

--- UDPClient.py
+++ UDPClient.py
@@ -1,9 +1,29 @@
 from socket import *
+
+#definindo o endereco do servidor
 serverName = 'localhost'
 serverPort = 12000
+#definindo o endereco do cliente
 clientSocket = socket(AF_INET, SOCK_DGRAM)
-message = input('Input lowercase sentence:')
-clientSocket.sendto(message.encode(),(serverName, serverPort))
-modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
-print(modifiedMessage.decode())
-clientSocket.close()
+# Create a UDP socket
+sock = socket(AF_INET, SOCK_DGRAM)
+server_address = (serverName, serverPort)
+# Send data
+filename = input("Digite o nome do arquivo: ")
+file = open(filename, "r+b")
+file_data = file.read(1024)
+file.close()
+sock.sendto(file_data, server_address)
+print("Enviado!")
+#receber o arquivo de volta do servidor
+file_data, server = sock.recvfrom(1024)
+print("Recebido!")
+file = open(file_data, "wb")
+file.write(file_data)
+file.close()
+print("Arquivo recebido!")
+# Close the socket
+sock.close()
+
+
+
--- UDPServer.py
+++ UDPServer.py
@@ -1,9 +1,24 @@
 from socket import *
-serverPort = 12000
+
+# Cria o socket UDP
 serverSocket = socket(AF_INET, SOCK_DGRAM)
-serverSocket.bind(('', serverPort))
-print("The server is ready to receive")
+serverSocket.bind(('', 12000))
+
+# Recebe o arquivo
 while True:
     message, clientAddress = serverSocket.recvfrom(2048)
-    modifiedMessage = message.decode().upper()
-    serverSocket.sendto(modifiedMessage.encode(),clientAddress)
+    filename = message.decode()
+    print('Arquivo recebido: ' + filename)
+    # MUDA O NOME DO ARQUIVO
+    filename = filename.split('.')
+    filename[0] = filename[0] + '_copia'
+    filename = '.'.join(filename)
+    print('Arquivo copiado: ' + filename)
+    f = open(filename, 'wb')
+    outputdata = f.read()
+    f.close()
+    serverSocket.sendto(outputdata, clientAddress)
+    print('Arquivo enviado: ' + filename)
+    serverSocket.close()
+    print('Fim do programa')
+    break
